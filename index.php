62d8a5d83ca126268488f7c3500143d368036e50
- <->
2025-01-27 10:05:45 -0300

    migração para php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+    <title>Pagina Dinamica</title>
+</head>
+<body>
+ <!------------------------------------------------Navebar----------------------------------------------------------------->
+
+ <?php include('./snippers/naverbar.html');?>
+        
+<!-------------------------------------------------Conteudo----------------------------------------------------------------->
+
+<?php include('./snippers/conteudo.html');?>
+
+<!-------------------------------------------------Tabela------------------------------------------------------------------->
+
+<?php include('./snippers/tabela.html');?>
+
+<!----------------------------------------------Grade de imagens------------------------------------------------------------->
+  
+<?php include('./snippers/grand_imagens.html');?>
+
+<!---------------------------------------------------Apoio-------------------------------------------------------------------->
+  
+<?php include('./snippers/apoio.html');?>
+
+<script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
